152736079d28178061ad13d4c8c29328c4ae6db7
Денис Тугин <tugindenis6@gmail.com>
2026-03-03 12:13:38 +0300

    Day-2 исправленна работа за Day-1

--- app/src/main/java/com/example/app/domain/model/RegisterRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app.domain.model
-
-data class RegisterRequest(
-    val email: String,
-    val password: String
-)
--- app/src/main/java/com/example/app/domain/model/RegisterResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app.domain.model
-
-data class RegisterResponse(
-    val id: String? = null,     // ID пользователя (если успешно)
-    val error: String? = null   // Текст ошибки (если не успешно)
-)
